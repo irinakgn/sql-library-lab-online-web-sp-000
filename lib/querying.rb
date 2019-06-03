10fdfcf529a482a21ba9b7923f90bd90dcca50e3
Irina Kagan <irinagamb@gmail.com>
2019-06-03 12:08:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,16 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "select name, motto from (SELECT name, motto, length(motto) FROM characters order by length(motto) desc limit 1)"
 end
+
+#SELECT city, length(city) FROM station order by length(city) desc limit 1;
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+"SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -16,7 +18,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM series JOIN characters ON series.author_id = characters.author_id
+   WHERE characters.species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
